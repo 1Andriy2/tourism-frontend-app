deb88661658e9172a8534d6d94e6472547eca2cb
Vovchick4 <vovaspodpivas@gmail.com>
2023-03-23 23:42:24 +0200

    feat: auth llogic emulations

---
+++ src/entities/viewer/model/use-private-route.ts
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom"
+
+import { useViewerAtom } from "."
+import { urls } from "../../../shared/config"
+
+export default function usePrivateRoute() {
+    const navigate = useNavigate()
+    const { isAuthenticated } = useViewerAtom()
+
+    useEffect(() => {
+        if (!isAuthenticated) {
+            navigate(urls.logIn)
+        }
+    }, [isAuthenticated])
+}

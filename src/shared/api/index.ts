26b151f1136984b7ef1dbfa4818a64ed4c2879e0
Andriy <69579499+1Andriy2@users.noreply.github.com>
2023-03-24 16:42:28 +0200

    add login and register authorization

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -1,4 +1,5 @@
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, addDoc } from "firebase/firestore";
+import { IAuthData, IUserData } from "../../entities/viewer/store";
 
 import { firestore } from '../../processes/firebase';
 
@@ -6,4 +7,9 @@ export const getViewers = async () => {
     const docs = (await getDocs(collection(firestore, "users"))).docs
     const users = docs.map(doc => doc.data())
     return users
+}
+
+export const addViewer = async (data: IAuthData) => {
+    const docs = (await addDoc(collection(firestore, "users"),data))
+    return docs
 }

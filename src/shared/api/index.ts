fe21e13d1851df18b8979bacc4f52d3e61c8065a
Andriy <andrijkostelnij72@gmail.com>
2023-03-25 18:26:25 +0200

    add mutate: register

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -1,8 +1,8 @@
-import { createUserWithEmailAndPassword, getAuth, UserCredential } from "firebase/auth";
-import { collection, getDocs, addDoc } from "firebase/firestore";
-import { IAuthData, IUserData } from "../../entities/viewer/store";
+import { getAuth, createUserWithEmailAndPassword, UserCredential, User } from "firebase/auth"
+import { collection, getDocs, addDoc, DocumentReference, DocumentData } from "firebase/firestore"
+import { IAuthData, IUserData } from "../../entities/viewer/store"
 
-import { firestore } from '../../processes/firebase';
+import { firestore } from '../../processes/firebase'
 
 export const getViewers = async () => {
     const docs = (await getDocs(collection(firestore, "users"))).docs
@@ -10,16 +10,15 @@ export const getViewers = async () => {
     return users
 }
 
-export const addViewer = async (data: IAuthData) => {
-    const docs = (await addDoc(collection(firestore, "users"),data))
+export const addViewer = async (data: IUserData) => {
+    const docs = (await addDoc(collection(firestore, "users"), data))
     return docs
 }
 
-export const registerViwer = async (data: IUserData): Promise<UserCredential | undefined> => {
-    try {
-        const auth = getAuth();
-        return await createUserWithEmailAndPassword(auth, data.email, data.password)
-    } catch (err) {
-        console.log((err as Error).message) 
-    }
+export const registerViwer = async (data: IUserData): Promise<{ viewer: IUserData, token: string | undefined } | undefined> => {
+    const auth = getAuth();
+    const created: UserCredential = await createUserWithEmailAndPassword(auth, data.email, data.password)
+    const user: User & { accessToken?: string } = created.user
+    await addViewer(data)
+    return { viewer: data, token: user?.accessToken }
 }

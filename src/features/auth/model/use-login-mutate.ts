d57d6448353455099535082e5ea5983d5ceef1a1
Andriy <69579499+1Andriy2@users.noreply.github.com>
2023-03-25 21:39:22 +0200

    feat: logIn and register

---
+++ src/features/auth/model/use-login-mutate.ts
@@ -0,0 +1,29 @@
+import { useMutation } from "react-query"
+import { logInViwer } from "../../../shared/api"
+import { useToastView } from "../../../shared/hooks"
+import { IUserData } from "../../../entities/viewer/store"
+import { useViewerAtom } from "../../../entities/viewer/model"
+
+export default function useLogInMutate(oMutationOpts = {}) {
+    const toast = useToastView()
+    const { setAuthData } = useViewerAtom()
+
+    return useMutation(
+        async (oCredentials: Omit<IUserData, "name">) => await logInViwer(oCredentials),
+        {
+            ...oMutationOpts,
+            onMutate: () => {
+                toast({ title: "Starting logIn." })
+            },
+            onSuccess: (data) => {
+                if (data?.token) {
+                    setAuthData({ token: data.token, data: data.viewer })
+                    toast({ title: "Account loginned." })
+                }
+            },
+            onError: (err) => {
+                toast({ title: (err as Error).message })
+            }
+        }
+    )
+}
--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -1,8 +1,11 @@
-import { getAuth, createUserWithEmailAndPassword, UserCredential, User } from "firebase/auth"
-import { collection, getDocs, addDoc, DocumentReference, DocumentData } from "firebase/firestore"
+import { async } from "@firebase/util"
+import { getAuth, createUserWithEmailAndPassword, UserCredential, User, signInWithEmailAndPassword, signOut } from "firebase/auth"
+import { collection, getDocs, addDoc, DocumentReference, DocumentData, getDoc, query, doc } from "firebase/firestore"
 import { IAuthData, IUserData } from "../../entities/viewer/store"
 
 import { firestore } from '../../processes/firebase'
+
+const auth = getAuth();
 
 export const getViewers = async () => {
     const docs = (await getDocs(collection(firestore, "users"))).docs
@@ -15,10 +18,25 @@ export const addViewer = async (data: IUserData) => {
     return docs
 }
 
+export const getViewerByEmail = async (email: string) => {
+    const docs = (await getDoc(doc(firestore, "users", email)))
+    return docs.data()
+}
+
 export const registerViwer = async (data: IUserData): Promise<{ viewer: IUserData, token: string | undefined } | undefined> => {
-    const auth = getAuth();
     const created: UserCredential = await createUserWithEmailAndPassword(auth, data.email, data.password)
     const user: User & { accessToken?: string } = created.user
     await addViewer(data)
     return { viewer: data, token: user?.accessToken }
+}
+
+export const logInViwer = async (data: Omit<IUserData, "name">): Promise<{ viewer: any, token: string | undefined } | undefined> => {
+    const created: UserCredential = await signInWithEmailAndPassword(auth, data.email, data.password)
+    const user: User & { accessToken?: string } = created.user
+    const viewer = await getViewerByEmail(data.email)
+    return { viewer, token: user?.accessToken }
+}
+
+export const signOutViewer = async () => {
+    return await signOut(auth)
 }

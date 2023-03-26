8121e4160e8b1ef145eb5a4bf10890004de22a6f
Vovchick4 <vovaspodpivas@gmail.com>
2023-03-26 02:23:03 +0200

    feat: get uset

---
+++ src/features/auth/model/use-fetch-viewer.ts
@@ -0,0 +1,22 @@
+import { useQuery } from "react-query";
+
+import { getViewer } from "../../../shared/api";
+import { useToastView } from "../../../shared/hooks";
+import { useViewerAtom } from "../../../entities/viewer/model";
+
+export default function useFetchViewer(oQueriesOpts = {}) {
+    const toast = useToastView()
+    const { isAuthenticated, removeAuthData } = useViewerAtom()
+
+    return useQuery({
+        ...oQueriesOpts,
+        queryKey: ["GetViewer"],
+        queryFn: () => getViewer(),
+        onSuccess: () => toast({ title: "Getted user" }),
+        onError: (err) => {
+            removeAuthData()
+            toast({ title: (err as Error).message })
+        },
+        enabled: isAuthenticated
+    })
+}

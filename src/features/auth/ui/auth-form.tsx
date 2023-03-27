8c957d8e028249c882dd802e2833ceae60e0f17d
Andriy <andrijkostelnij72@gmail.com>
2023-03-27 20:15:44 +0300

    feat: account profile, feat: aside setups

--- src/features/auth/ui/auth-form.tsx
+++ src/features/auth/ui/auth-form.tsx
@@ -1,7 +1,9 @@
-import LogInForm from "./login-form";
-import RegisterForm from "./register-form";
+import LogInForm from "./login-form"
+import ProfileForm from "./profile-form"
+import RegisterForm from "./register-form"
 
 export default function AuthForm() { }
 
+AuthForm.ProfileTemplate = ProfileForm
 AuthForm.LoginTemplate = LogInForm
 AuthForm.RegisterTemplate = RegisterForm

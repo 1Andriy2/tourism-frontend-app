e11ff0a213437a7057ab4f65cb32d0a5a895d0cc
Vovchick4 <vovaspodpivas@gmail.com>
2023-04-03 01:21:30 +0300

    add tours page cards

--- src/pages/home/ui.tsx
+++ src/pages/home/ui.tsx
@@ -39,42 +39,12 @@ export default function HomePage() {
           </Button>
         </Box>
       </animated.div> */}
-
-      <Box minH="calc(100vh - 123px)">
-        <Parallax className="scroll" pages={4} style={{ top: '0', maxWidth: 1000, margin: "0 auto" }}>
-          <ParallaxLayer offset={0} speed={0.5}>
-            <Box minHeight="calc(100vh - 123px)" px="10px" display="flex" flexDirection="column" justifyContent="space-evenly">
-              <Heading size="3xl">
-                Tourism
-              </Heading>
-              <Heading size="lg" fontWeight="normal" lineHeight="50px">
-                <Highlight
-                  query={["dolor", "natus", "libero"]}
-                  styles={{ px: "2", py: "1", rounded: "full", bg: "whatsapp.400", color: isLight ? "white" : "black" }}
-                >
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
-                  ratione sapiente alias tempore recusandae libero, dolorem inventore
-                  enim labore perspiciatis. Ea reprehenderit fuga accusantium natus ab
-                  aspernatur cum. Impedit, minus?
-                </Highlight>
-              </Heading>
-              <Button variant="outline" colorScheme="whatsapp" borderRadius="20px" size="lg">
-                Get tourism
-              </Button>
-            </Box>
-          </ParallaxLayer>
-
-          <ParallaxLayer offset={0} speed={0.8} style={{ filter: "grayscale(80%) opacity(25%)", zIndex: -1 }}>
-            <Image
-              minH="100vh"
-              w="100%"
-              objectFit='cover'
-              src={nature3}
-              alt='Dan Abramov'
-            />
-          </ParallaxLayer>
-
-          <ParallaxLayer offset={1.5} speed={0.5}>
+      <Parallax pages={4} style={{ top: '0', maxWidth: 1160, margin: "0 auto" }}>
+        <ParallaxLayer offset={0} speed={0.5}>
+          <Box minHeight="calc(100vh - 123px)" px="10px" display="flex" flexDirection="column" justifyContent="space-evenly">
+            <Heading size="3xl">
+              Tourism
+            </Heading>
             <Heading size="lg" fontWeight="normal" lineHeight="50px">
               <Highlight
                 query={["dolor", "natus", "libero"]}
@@ -86,71 +56,98 @@ export default function HomePage() {
                 aspernatur cum. Impedit, minus?
               </Highlight>
             </Heading>
-          </ParallaxLayer>
+            <Button variant="outline" colorScheme="whatsapp" borderRadius="20px" size="lg">
+              Get tourism
+            </Button>
+          </Box>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={0} speed={0.8} style={{ filter: "grayscale(80%) opacity(25%)", zIndex: -1 }}>
+          <Image
+            minH="100vh"
+            w="100%"
+            objectFit='cover'
+            src={nature3}
+            alt='Dan Abramov'
+          />
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={1.5} speed={0.5}>
+          <Heading size="lg" fontWeight="normal" lineHeight="50px">
+            <Highlight
+              query={["dolor", "natus", "libero"]}
+              styles={{ px: "2", py: "1", rounded: "full", bg: "whatsapp.400", color: isLight ? "white" : "black" }}
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+              ratione sapiente alias tempore recusandae libero, dolorem inventore
+              enim labore perspiciatis. Ea reprehenderit fuga accusantium natus ab
+              aspernatur cum. Impedit, minus?
+            </Highlight>
+          </Heading>
+        </ParallaxLayer>
 
 
-          <ParallaxLayer offset={1.5} speed={0.8} style={{ filter: "grayscale(80%) opacity(25%)", zIndex: -1 }}>
-            <Image
-              minH="100vh"
-              w="100%"
-              objectFit='cover'
-              src={nature4}
-              alt='Dan Abramov'
-            />
-          </ParallaxLayer>
+        <ParallaxLayer offset={1.5} speed={0.8} style={{ filter: "grayscale(80%) opacity(25%)", zIndex: -1 }}>
+          <Image
+            minH="100vh"
+            w="100%"
+            objectFit='cover'
+            src={nature4}
+            alt='Dan Abramov'
+          />
+        </ParallaxLayer>
 
-          <ParallaxLayer offset={2.5} speed={0.5}>
-            <Heading size="lg" fontWeight="normal" lineHeight="50px">
-              <Highlight
-                query={["dolor", "natus", "libero"]}
-                styles={{ px: "2", py: "1", rounded: "full", bg: "whatsapp.400", color: isLight ? "white" : "black" }}
-              >
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
-                ratione sapiente alias tempore recusandae libero, dolorem inventore
-                enim labore perspiciatis. Ea reprehenderit fuga accusantium natus ab
-                aspernatur cum. Impedit, minus?
-              </Highlight>
-            </Heading>
-          </ParallaxLayer>
-
-
-          <ParallaxLayer offset={2.5} speed={0.8} style={{ filter: "grayscale(80%) opacity(25%)", zIndex: -1 }}>
-            <Image
-              minH="100vh"
-              w="100%"
-              objectFit='cover'
-              src={nature1}
-              alt='Dan Abramov'
-            />
-          </ParallaxLayer>
+        <ParallaxLayer offset={2.5} speed={0.5}>
+          <Heading size="lg" fontWeight="normal" lineHeight="50px">
+            <Highlight
+              query={["dolor", "natus", "libero"]}
+              styles={{ px: "2", py: "1", rounded: "full", bg: "whatsapp.400", color: isLight ? "white" : "black" }}
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+              ratione sapiente alias tempore recusandae libero, dolorem inventore
+              enim labore perspiciatis. Ea reprehenderit fuga accusantium natus ab
+              aspernatur cum. Impedit, minus?
+            </Highlight>
+          </Heading>
+        </ParallaxLayer>
 
 
-          <ParallaxLayer offset={3} speed={0.5}>
-            <Heading size="lg" fontWeight="normal" lineHeight="50px">
-              <Highlight
-                query={["dolor", "natus", "libero"]}
-                styles={{ px: "2", py: "1", rounded: "full", bg: "whatsapp.400", color: isLight ? "white" : "black" }}
-              >
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
-                ratione sapiente alias tempore recusandae libero, dolorem inventore
-                enim labore perspiciatis. Ea reprehenderit fuga accusantium natus ab
-                aspernatur cum. Impedit, minus?
-              </Highlight>
-            </Heading>
-          </ParallaxLayer>
+        <ParallaxLayer offset={2.5} speed={0.8} style={{ filter: "grayscale(80%) opacity(25%)", zIndex: -1 }}>
+          <Image
+            minH="100vh"
+            w="100%"
+            objectFit='cover'
+            src={nature1}
+            alt='Dan Abramov'
+          />
+        </ParallaxLayer>
 
 
-          <ParallaxLayer offset={3} speed={0.8} style={{ filter: "grayscale(80%) opacity(25%)", zIndex: -1 }}>
-            <Image
-              minH="100vh"
-              w="100%"
-              objectFit='cover'
-              src={nature2}
-              alt='Dan Abramov'
-            />
-          </ParallaxLayer>
-        </Parallax>
-      </Box>
+        <ParallaxLayer offset={3} speed={0.5}>
+          <Heading size="lg" fontWeight="normal" lineHeight="50px">
+            <Highlight
+              query={["dolor", "natus", "libero"]}
+              styles={{ px: "2", py: "1", rounded: "full", bg: "whatsapp.400", color: isLight ? "white" : "black" }}
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+              ratione sapiente alias tempore recusandae libero, dolorem inventore
+              enim labore perspiciatis. Ea reprehenderit fuga accusantium natus ab
+              aspernatur cum. Impedit, minus?
+            </Highlight>
+          </Heading>
+        </ParallaxLayer>
+
+
+        <ParallaxLayer offset={3} speed={0.8} style={{ filter: "grayscale(80%) opacity(25%)", zIndex: -1 }}>
+          <Image
+            minH="100vh"
+            w="100%"
+            objectFit='cover'
+            src={nature2}
+            alt='Dan Abramov'
+          />
+        </ParallaxLayer>
+      </Parallax>
     </Fragment>
   );
 }

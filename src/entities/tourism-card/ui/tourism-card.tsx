5849ae8d864c4b24e8e485f431ca7e26ee7548e2
Andriy <andrijkostelnij72@gmail.com>
2023-04-21 01:13:34 +0300

    fix

--- src/entities/tourism-card/ui/tourism-card.tsx
+++ src/entities/tourism-card/ui/tourism-card.tsx
@@ -25,87 +25,88 @@ export interface IToursimPlacesCard {
 }
 
 
-const TourismCard: ForwardRefRenderFunction<unknown, IToursimPlacesCard> = ({ user, place: { preview, title, description } }, ref: LegacyRef<any> | undefined) => {
-    const toast = useToastView()
-    const { mutate, isLoading } = useMutation(
-        ["markPlace"],
-        async () => await markPlace(title, user),
-        {
-            onSuccess: () => {
-                toast({ title: "Change Marked place" })
-                queryClient.invalidateQueries("GetViewer")
-            }
-        })
+const TourismCard: ForwardRefRenderFunction<unknown, IToursimPlacesCard> =
+    ({ user, place: { preview, title, description } }, ref: LegacyRef<any> | undefined) => {
+        const toast = useToastView()
+        const { mutate, isLoading } = useMutation(
+            ["markPlace"],
+            async () => await markPlace(title, user),
+            {
+                onSuccess: () => {
+                    toast({ title: "Change Marked place" })
+                    queryClient.invalidateQueries("GetViewer")
+                }
+            })
 
-    return (
-        <Card ref={ref} position="relative" variant="filled">
-            <CardHeader>
-                <Center>
-                    <Tooltip label={title}>
-                        <Heading size="lg" noOfLines={1}>
-                            {title}
-                        </Heading>
-                    </Tooltip>
-                </Center>
-            </CardHeader>
-            <CardBody overflow="hidden">
-                <Box>
-                    <Image
-                        className={styles.image_preview}
-                        minH="200px"
-                        maxH="200px"
-                        h="100%"
-                        w="100%"
-                        objectFit="cover"
-                        borderRadius="8px"
-                        src={preview}
-                    />
-                    <Divider my={5} />
-                    <Accordion allowMultiple>
-                        <AccordionItem>
-                            <AccordionButton>
-                                <Heading size="md">
-                                    {title} (more)
-                                </Heading>
-                            </AccordionButton>
-                            <Tooltip label={description}>
-                                <AccordionPanel noOfLines={2}>
-                                    {description}
-                                </AccordionPanel>
-                            </Tooltip>
-                        </AccordionItem>
-                    </Accordion>
+        return (
+            <Card ref={ref} position="relative" variant="filled">
+                <CardHeader>
+                    <Center>
+                        <Tooltip label={title}>
+                            <Heading size="lg" noOfLines={1}>
+                                {title}
+                            </Heading>
+                        </Tooltip>
+                    </Center>
+                </CardHeader>
+                <CardBody overflow="hidden">
+                    <Box>
+                        <Image
+                            className={styles.image_preview}
+                            minH="200px"
+                            maxH="200px"
+                            h="100%"
+                            w="100%"
+                            objectFit="cover"
+                            borderRadius="8px"
+                            src={preview}
+                        />
+                        <Divider my={5} />
+                        <Accordion allowMultiple>
+                            <AccordionItem>
+                                <AccordionButton>
+                                    <Heading size="md">
+                                        {title} (more)
+                                    </Heading>
+                                </AccordionButton>
+                                <Tooltip label={description}>
+                                    <AccordionPanel noOfLines={2}>
+                                        {description}
+                                    </AccordionPanel>
+                                </Tooltip>
+                            </AccordionItem>
+                        </Accordion>
 
-                    <Box position="absolute" top="8px" right="8px">
-                        <Tag colorScheme="red">
-                            <TagLabel>Lviv</TagLabel>
-                        </Tag>
+                        <Box position="absolute" top="8px" right="8px">
+                            <Tag colorScheme="red">
+                                <TagLabel>Lviv</TagLabel>
+                            </Tag>
+                        </Box>
                     </Box>
-                </Box>
-            </CardBody>
-            <CardFooter justifyContent="space-evenly">
-                <Tooltip label="Mark">
-                    <IconButton
-                        isLoading={isLoading}
-                        isDisabled={user === null}
-                        colorScheme="red"
-                        variant={user?.places.includes(title) ? "solid" : "outline"}
-                        icon={<StarIcon />}
-                        aria-label={"FollowMe"}
-                        onClick={() => mutate()}
-                    />
-                </Tooltip>
-                <Tooltip label="Info">
-                    <IconButton
-                        colorScheme="red"
-                        variant="outline"
-                        icon={<InfoIcon />}
-                        aria-label={"FollowMe"}
-                    />
-                </Tooltip>
-            </CardFooter>
-        </Card>
-    )
-}
+                </CardBody>
+                <CardFooter justifyContent="space-evenly">
+                    <Tooltip label="Mark">
+                        <IconButton
+                            isLoading={isLoading}
+                            isDisabled={user === null}
+                            colorScheme="red"
+                            variant={user?.places.includes(title) ? "solid" : "outline"}
+                            icon={<StarIcon />}
+                            aria-label={"FollowMe"}
+                            onClick={() => mutate()}
+                        />
+                    </Tooltip>
+                    <Tooltip label="Info">
+                        <IconButton
+                            colorScheme="red"
+                            variant="outline"
+                            icon={<InfoIcon />}
+                            aria-label={"FollowMe"}
+                        />
+                    </Tooltip>
+                </CardFooter>
+            </Card>
+        )
+    }
 
 export default TourismCard

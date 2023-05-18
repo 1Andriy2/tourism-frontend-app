0d3bd62b4938a0c587c0a9407be693afdbb0bba7
Andriy <andrijkostelnij72@gmail.com>
2023-05-19 00:05:47 +0300

    fix: map react-leaglet

--- src/pages/contacts/ui.tsx
+++ src/pages/contacts/ui.tsx
@@ -1,9 +1,18 @@
-import "leaflet/dist/leaflet.css";
-import { MapContainer} from "react-leaflet";
+import "leaflet/dist/leaflet.css"
+import { Popup, Marker, TileLayer, MapContainer } from "react-leaflet"
 
-export default function App() {
+export default function Contacts() {
   return (
-    <MapContainer center={[48.8566, 2.3522]} zoom={13}>
+    <MapContainer id="ContactsMAPA" center={[51.505, -0.09]} zoom={13} style={{ width: "100%", height: "calc(100vh - 120px)" }}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
     </MapContainer>
   );
 }

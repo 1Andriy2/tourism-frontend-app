fd1dedf4283b0264c4a568a674c6efe1ced37cdc
Andriy <andrijkostelnij72@gmail.com>
2023-06-05 19:56:44 +0300

    abort

--- src/features/auth/ui/login-form.tsx
+++ src/features/auth/ui/login-form.tsx
@@ -2,7 +2,6 @@ import { Link as ReachLink } from "react-router-dom";
 import { useFormik } from "formik";
 import { ArrowLeftIcon } from "@chakra-ui/icons";
 import {
-<<<<<<< HEAD
     FormControl,
     FormLabel,
     Input,
@@ -11,16 +10,6 @@ import {
     VStack,
     Flex,
     Box,
-=======
-  FormControl,
-  FormLabel,
-  Input,
-  Button,
-  FormErrorMessage,
-  VStack,
-  Flex,
-  Box,
->>>>>>> 7479b66284716956212e7746683e6adccee580dc
 } from "@chakra-ui/react";
 
 import { urls } from "../../../shared/config";
@@ -30,7 +19,6 @@ import useLogInMutate from "../model/use-login-mutate";
 import useResetMutate from "../model/use-reset-password";
 
 export default function LogInForm() {
-<<<<<<< HEAD
     const { mutate, isLoading } = useLogInMutate();
     const { mutate: resetMutate } = useResetMutate();
 
@@ -95,70 +83,4 @@ export default function LogInForm() {
             </VStack>
         </form>
     );
-=======
-  const { mutate, isLoading } = useLogInMutate();
-  const { mutate: resetMutate }  = useResetMutate();
-
-  const formik = useFormik({
-    initialValues: LOG_IN_STATE,
-    validationSchema: LogInSchema,
-    onReset: () => {},
-    onSubmit: (values) => {
-      mutate(values);
-    },
-  });
-
-  return (
-    <form
-      onSubmit={formik.handleSubmit}
-      onReset={formik.handleReset}
-      style={{ width: "100%", padding: "20px" }}
-    >
-      <Button as={ReachLink} to={urls.home} leftIcon={<ArrowLeftIcon />} mb={5}>
-        Return to home page
-      </Button>
-
-      <VStack spacing={5}>
-        <FormControl
-          isInvalid={!!(formik.errors.email && formik.touched.email)}
-        >
-          <FormLabel>Input Email:</FormLabel>
-          <Input
-            type="email"
-            name="email"
-            isRequired
-            placeholder="Input Email"
-            value={formik.values.email}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-          />
-          <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
-        </FormControl>
-        <FormControl
-          isInvalid={!!(formik.errors.password && formik.touched.password)}
-        >
-          <FormLabel>Input Password:</FormLabel>
-          <Input
-            type="password"
-            name="password"
-            isRequired
-            placeholder="Input Password"
-            value={formik.values.password}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-          />
-          <FormErrorMessage>{formik.errors.password}</FormErrorMessage>
-        </FormControl>
-        <Box width="100%" display="flex" justifyContent="right">
-            <Button type="button" onClick={() => resetMutate(formik.values.email)} variant="link" colorScheme="teal">
-              Forgot Password?
-            </Button>
-        </Box>
-        <Button type="submit" colorScheme="teal" w="100%">
-          Submit
-        </Button>
-      </VStack>
-    </form>
-  );
->>>>>>> 7479b66284716956212e7746683e6adccee580dc
 }
